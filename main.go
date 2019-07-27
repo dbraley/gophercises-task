2800ee540a29c61d13842ef3259f3dd9e0297305
David Braley <david.braley@gmail.com>
2019-07-26 20:07:09 -0400

    Create DB and init bucket

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/dbraley/gophercises-task/cmd"
+import (
+	"fmt"
+	"github.com/dbraley/gophercises-task/cmd"
+	"github.com/dbraley/gophercises-task/db"
+	"github.com/mitchellh/go-homedir"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
